e5dc80c8a0fc23dc7c6ff88b303d0675426f6bc9
Quan Pham <doraemonfanclub@gmail.com>
2021-02-10 20:49:32 +0700

    Add local env

--- src/main/java/com/clipsub/steamapps/SteamAppsApplication.java
+++ src/main/java/com/clipsub/steamapps/SteamAppsApplication.java
@@ -1,18 +1,11 @@
 package com.clipsub.steamapps;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SteamAppsApplication implements CommandLineRunner {
+public class SteamAppsApplication {
     public static void main(String[] args) {
         SpringApplication.run(SteamAppsApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        ObjectMapper objectMapper = new ObjectMapper();
     }
 }
